bc9f81c366f738cefaef68b5e03e38a1e43b0025
Alexander Hartmann <hartmannaf@googlemail.com>
2016-02-19 03:04:38 +0100

    added bootloaderloader for all people who doesn't want to reinstall arm9loaderhax or doesn't have a hardmod.

---
+++ bootloader/bootloaderloader/source/main.c
@@ -0,0 +1,27 @@
+#include "common.h"
+#include "sdmmc.h"
+#include "i2c.h"
+#include "fatfs/ff.h"
+
+#define BOOTLOADER_PAYLOAD_ADDRESS	0x24F00000
+#define BOOTLOADER_PAYLOAD_SIZE		0x00100000
+
+
+int main()
+{
+	FATFS fs;
+	FIL payload;
+	u32 br;
+	
+	if(f_mount(&fs, "0:", 0) == FR_OK)
+	{
+		if(f_open(&payload, "arm9bootloader.bin", FA_READ | FA_OPEN_EXISTING) == FR_OK)
+		{
+			f_read(&payload, BOOTLOADER_PAYLOAD_ADDRESS, BOOTLOADER_PAYLOAD_SIZE, &br);
+			((void (*)())BOOTLOADER_PAYLOAD_ADDRESS)();
+		}
+	}
+	
+	i2cWriteRegister(I2C_DEV_MCU, 0x20, (u8)(1<<0));
+    return 0;
+}
